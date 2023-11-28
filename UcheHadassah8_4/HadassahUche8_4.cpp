18bf650703bd4d758f878b37b9165118f91ffead
Hadassah Uche <uhadassah1@gmail.com>
2023-11-28 13:41:56 +0200

    Add project files.

---
+++ UcheHadassah8_4/HadassahUche8_4.cpp
@@ -0,0 +1,8 @@
+/*Counting letters: Use pointer arithmetic to write a program that reads a word and a 
+letter from the user and counts the number of times the letter occurred in the string. The 
+program should output the index position(s) where it found the letter, as well as print the 
+number of times it found it. Example:
+Enter a word: anna
+Enter a letter: n
+Letter found at index position(s) 1, 2
+Letter found 2 times.*/
