b5d38d16d18769582e402ed5944bff4337e06b95
Hadassah Uche <uhadassah1@gmail.com>
2023-11-28 14:07:42 +0200

    Cleaned up the code and added relevant comments

--- UcheHadassah8_4/HadassahUche8_4.cpp
+++ UcheHadassah8_4/HadassahUche8_4.cpp
@@ -1,11 +1,6 @@
-/*Counting letters: Use pointer arithmetic to write a program that reads a word and a 
-letter from the user and counts the number of times the letter occurred in the string. The 
-program should output the index position(s) where it found the letter, as well as print the 
-number of times it found it. Example:
-Enter a word: anna
-Enter a letter: n
-Letter found at index position(s) 1, 2
-Letter found 2 times.*/
+/*Name:Uche Hadassah
+This program reads a word and a letter from the user, counts and outputs how many times the 
+letter appeared in the word as well as the positions*/
 #include<iostream>
 #include<cstring>
 using namespace std;
@@ -17,7 +12,7 @@ int main()
 	char* p = word;
 	cout << "Enter a word:";
 	cin >> word;
-	cout << "Enter the letter you wish to search for:";
+	cout << "Enter a letter to search for:";
 	cin >> letter;
 	int count = 0;
 	while (*p != '\0')
@@ -26,12 +21,18 @@ int main()
 			count++;
 		p++;
 	}
-	p = word;//Reset the pointer
-	cout << " The letter " << letter << " appeared " << count << " times in the word, at position(s) ";
-	for(int i = 0;i<strlen(word);i++)
+	if (count == 0)
+		cout << letter << " does not appear in the word.";
+	else
 	{
-		if (*p == letter)
-			cout << i <<" ";
-		p++;
+		p = word;//Reset the pointer
+		cout << "Letter found at index position(s) ";
+		for (int i = 0; i < strlen(word); i++)
+		{
+			if (*p == letter)
+				cout << i << ",";
+			p++;
+		}
+		cout << "Letter found " << count << " times.";
 	}
 }
